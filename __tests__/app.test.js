6a2e066377ccd222737ccdba7064e97667758e44
Bradley-Bird <Bradleybird2@icloud.com>
2022-06-09 11:07:05 -0700

    test for character by id passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -13,6 +13,11 @@ describe('character routes', () => {
     const expected = characters.map((character) => character);
     expect(resp.body).toEqual(expected);
   });
+  it('/it should return detail view of the character by id', async () => {
+    const resp = await request(app).get('/api/v1/characters/1');
+    const expected = characters[0];
+    expect(resp.body).toEqual(expected);
+  });
 
   afterAll(() => {
     pool.end();
