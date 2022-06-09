18824891c797af67b4d61c464c69c4d7bd1d3761
Bradley-Bird <Bradleybird2@icloud.com>
2022-06-08 17:12:02 -0700

    created db

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,15 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP table if exists sanderson_characters;
+
+CREATE table characters (
+    id BIGINT GENERATED ALWAYS AS IDENTITY, 
+    name VARCHAR NOT NULL, 
+    series VARCHAR NOT NULL, 
+    alias VARCHAR
+); 
+
+INSERT INTO characters (name, series, alias) VALUES
+('Kaladin Storm-Blessed',  'The Storm-Light Archive', null), 
+('Vin', 'Mistborn', 'Valette Renoux'),
+('Raoden', 'Elantris', null);
