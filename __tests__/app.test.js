efe6d4d526d73df305760cd9842e57e9c1c3a87b
Bradley-Bird <Bradleybird2@icloud.com>
2022-06-09 11:03:55 -0700

    first test passing for all characters path

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,13 +2,16 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const { characters } = require('../data/characters');
 
-describe('backend-express-template routes', () => {
+describe('character routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('/it should return a list of characters', async () => {
+    const resp = await request(app).get('/api/v1/characters');
+    const expected = characters.map((character) => character);
+    expect(resp.body).toEqual(expected);
   });
 
   afterAll(() => {
