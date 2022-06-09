f9dc60b875a2cd748dff425c21fc6cf71a7051df
Bradley-Bird <Bradleybird2@icloud.com>
2022-06-08 17:46:31 -0700

    started Character class

---
+++ data/characters.js
@@ -0,0 +1,12 @@
+const characters = [
+  {
+    id: '1',
+    name: 'Kaladin Storm-Blessed',
+    series: 'The Storm-Light Archive',
+    alias: null,
+  },
+  { id: '2', name: 'Vin', series: 'Mistborn', alias: 'Valette Renoux' },
+  { id: '3', name: 'Raoden', series: 'Elantris', alias: null },
+];
+
+module.exports = { characters };
